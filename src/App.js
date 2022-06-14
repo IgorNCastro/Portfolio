c189df2d6ba044dfb4695f3a26133fc8001680db
Igor Castro <ncastro.igor@gmail.com>
2022-06-14 13:42:00 -0300

    Final adequations

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ import redux from "./gallery/icons/redux.png";
 import git from "./gallery/icons/git.png";
 import terminal from "./gallery/icons/terminal.png";
 import linux from "./gallery/icons/linux.png";
+import jest from "./gallery/icons/jest.png";
+import tailwind from "./gallery/icons/tailwind.png";
+import gifMaker from "./gallery/screenshots/gifMaker.gif";
+import footguess from "./gallery/screenshots/footguess.png";
+import trivia from "./gallery/screenshots/trivia.png";
+import tryunfo from "./gallery/screenshots/tryunfo.png";
+import wallet from "./gallery/screenshots/wallet.png";
+import solarSystem from "./gallery/screenshots/solarSystem.png";
+import ticTacToe from "./gallery/screenshots/ticTacToe.png";
 
 function App() {
   return (
@@ -62,6 +71,13 @@ function App() {
               My name is Igor Castro and I'm a Software Developer.
               "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
             </h4>
+            <div>
+              <img
+                className="gif-image object-cover w-72 h-72"
+                src={gifMaker}
+                alt="Gif Igor"
+              />
+            </div>
           </div>
         </article>
         <div className="portfolio-header">
@@ -70,14 +86,14 @@ function App() {
         <section className="portfolio-section">
           <div className="block">
             <img
-              className="object-cover w-full h-52"
-              src="https://www.hyperui.dev/photos/art-1.jpeg"
-              alt=""
+              className="object-cover w-full h-64"
+              src={footguess}
+              alt="Screenshot of Footguess site"
             />
-            <h5 className="mt-4 text-xl font-bold text-gray-900">Footguess</h5>
-            <p className="max-w-sm mt-2 text-gray-700">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni reiciendis
-              sequi ipsam incidunt.
+            <h5 className="mt-2 text-xl font-bold text-gray-900">Footguess</h5>
+            <p className="max-w-sm text-gray-700 text-justify mx-0.5">
+              Game developed in React that gives the user six chances to discover who the mystery soccer player of the day is.
+              For that it gives clues based on previous guesses that came close to the answer.
             </p>
             <div className="card-actions">
               <a target="_blank" href="https://github.com/IgorNCastro/project-footguess">
@@ -90,14 +106,14 @@ function App() {
           </div>
           <div className="block">
             <img
-              className="object-cover w-full h-52"
-              src="https://www.hyperui.dev/photos/art-1.jpeg"
-              alt=""
+              className="object-cover w-full h-64"
+              src={trivia}
+              alt="Screenshot of Trivia site"
             />
-            <h5 className="mt-4 text-xl font-bold text-gray-900">Trivia</h5>
-            <p className="max-w-sm mt-2 text-gray-700">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni reiciendis
-              sequi ipsam incidunt.
+            <h5 className="mt-2 text-xl font-bold text-gray-900">Trivia</h5>
+            <p className="max-w-sm text-gray-700 text-justify mx-0.5">
+              Game developed in React Redux in which the user has to answer five questions of any theme in less than 30 seconds.
+              After that is presented a feedback message and ranking screen.
             </p>
             <div className="card-actions">
               <a target="_blank" href="https://github.com/IgorNCastro/project-trivia">
@@ -110,14 +126,14 @@ function App() {
           </div>
           <div className="block">
             <img
-              className="object-cover w-full h-52"
-              src="https://www.hyperui.dev/photos/art-1.jpeg"
-              alt=""
+              className="object-cover w-96 h-64"
+              src={wallet}
+              alt="Screenshot of Wallet site"
             />
-            <h5 className="mt-4 text-xl font-bold text-gray-900">Wallet</h5>
-            <p className="max-w-sm mt-2 text-gray-700">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni reiciendis
-              sequi ipsam incidunt.
+            <h5 className="mt-2 text-xl font-bold text-gray-900">Wallet</h5>
+            <p className="max-w-sm text-gray-700 text-justify my-px">
+              Website developed in React Redux where the user can control their budgets by inserting different expenses.
+              Uses an API to get the exchange rate of various currencies.
             </p>
             <div className="card-actions">
               <a target="_blank" href="https://github.com/IgorNCastro/project-wallet">
@@ -130,14 +146,14 @@ function App() {
           </div>
           <div className="block">
             <img
-              className="object-cover w-full h-52"
-              src="https://www.hyperui.dev/photos/art-1.jpeg"
-              alt=""
+              className="object-cover w-full h-64"
+              src={tryunfo}
+              alt="Screenshot of Tryunfo site"
             />
-            <h5 className="mt-4 text-xl font-bold text-gray-900">Tryunfo</h5>
-            <p className="max-w-sm mt-2 text-gray-700">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni reiciendis
-              sequi ipsam incidunt.
+            <h5 className="mt-2 text-xl font-bold text-gray-900">Tryunfo</h5>
+            <p className="max-w-sm text-gray-700 text-justify mx-0.5">
+              Website developed in React that allows the user to create cards of NBA players, giving them points for different attributes and rarity.
+              After is posible to filter the deck of cards by name or rarity.
             </p>
             <div className="card-actions">
               <a target="_blank" href="https://github.com/IgorNCastro/project-tryunfo">
@@ -150,14 +166,13 @@ function App() {
           </div>
           <div className="block">
             <img
-              className="object-cover w-full h-52"
-              src="https://www.hyperui.dev/photos/art-1.jpeg"
-              alt=""
+              className="object-cover w-96 h-72"
+              src={solarSystem}
+              alt="Screenshot of Solar System site"
             />
-            <h5 className="mt-4 text-xl font-bold text-gray-900">Solar System</h5>
-            <p className="max-w-sm mt-2 text-gray-700">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni reiciendis
-              sequi ipsam incidunt.
+            <h5 className="mt-2 text-xl font-bold text-gray-900">Solar System</h5>
+            <p className="max-w-sm text-gray-700 text-justify my-px">
+              Website developed in React that uses an API to show all the planets in our Solar System and every recorded mission to space.
             </p>
             <div className="card-actions">
               <a target="_blank" href="https://github.com/IgorNCastro/project-solar-system">
@@ -170,14 +185,14 @@ function App() {
           </div>
           <div className="block">
             <img
-              className="object-cover w-full h-52"
-              src="https://www.hyperui.dev/photos/art-1.jpeg"
-              alt=""
+              className="object-cover w-full h-72"
+              src={ticTacToe}
+              alt="Screenshot of Tic Tac Toe site"
             />
-            <h5 className="mt-4 text-xl font-bold text-gray-900">Tic-Tac-Toe</h5>
-            <p className="max-w-sm mt-2 text-gray-700">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni reiciendis
-              sequi ipsam incidunt.
+            <h5 className="mt-2 text-xl font-bold text-gray-900">Tic-Tac-Toe</h5>
+            <p className="max-w-sm text-gray-700 text-justify mx-0.5">
+              Game where you can play against a friend or the AI.
+              Developed as the final project of CS50, a Harvard University course available on edX.
             </p>
             <div className="card-actions">
               <a target="_blank" href="https://github.com/IgorNCastro/project-tic-tac-toe">
@@ -266,6 +281,30 @@ function App() {
           <div className="block-skills">
             <img
               className="object-cover w-28 h-28 shadow-xl rounded-xl"
+              src={tailwind}
+              alt="Tailwind"
+            />
+            <div className="p-2">
+              <h5 className="text-xl font-bold text-gray-900">
+                Tailwind
+              </h5>
+            </div>
+          </div>
+          <div className="block-skills">
+            <img
+              className="object-cover w-28 h-28 shadow-xl rounded-xl"
+              src={jest}
+              alt="Jest"
+            />
+            <div className="p-2">
+              <h5 className="text-xl font-bold text-gray-900">
+                Jest
+              </h5>
+            </div>
+          </div>
+          <div className="block-skills">
+            <img
+              className="object-cover w-28 h-28 shadow-xl rounded-xl"
               src={terminal}
               alt="Terminal"
             />
@@ -291,22 +330,22 @@ function App() {
         <section className="contact-section">
           <h5 className="mx-2 mt-2 font-medium text-gray-800 dark:text-gray-200 text-center" id="contact">Contact me</h5>
           <div className="soc">
-            <a href="#" className="icon facebook" title="Facebook">
+            <a href="https://www.facebook.com/castroigor" target="_blank" className="icon facebook" title="Facebook">
               <svg viewBox="0 0 512 512">
                 <path d="M211.9 197.4h-36.7v59.9h36.7V433.1h70.5V256.5h49.2l5.2-59.1h-54.4c0 0 0-22.1 0-33.7 0-13.9 2.8-19.5 16.3-19.5 10.9 0 38.2 0 38.2 0V82.9c0 0-40.2 0-48.8 0 -52.5 0-76.1 23.1-76.1 67.3C211.9 188.8 211.9 197.4 211.9 197.4z"/>
               </svg>
             </a>
-            <a href="#" className="icon github" title="GitHub">
+            <a href="https://github.com/IgorNCastro" target="_blank" className="icon github" title="GitHub">
               <svg viewBox="71 71 370 370">
                 <path d="M256 70.7c-102.6 0-185.9 83.2-185.9 185.9 0 82.1 53.3 151.8 127.1 176.4 9.3 1.7 12.3-4 12.3-8.9V389.4c-51.7 11.3-62.5-21.9-62.5-21.9 -8.4-21.5-20.6-27.2-20.6-27.2 -16.9-11.5 1.3-11.3 1.3-11.3 18.7 1.3 28.5 19.2 28.5 19.2 16.6 28.4 43.5 20.2 54.1 15.4 1.7-12 6.5-20.2 11.8-24.9 -41.3-4.7-84.7-20.6-84.7-91.9 0-20.3 7.3-36.9 19.2-49.9 -1.9-4.7-8.3-23.6 1.8-49.2 0 0 15.6-5 51.1 19.1 14.8-4.1 30.7-6.2 46.5-6.3 15.8 0.1 31.7 2.1 46.6 6.3 35.5-24 51.1-19.1 51.1-19.1 10.1 25.6 3.8 44.5 1.8 49.2 11.9 13 19.1 29.6 19.1 49.9 0 71.4-43.5 87.1-84.9 91.7 6.7 5.8 12.8 17.1 12.8 34.4 0 24.9 0 44.9 0 51 0 4.9 3 10.7 12.4 8.9 73.8-24.6 127-94.3 127-176.4C441.9 153.9 358.6 70.7 256 70.7z"/>
               </svg>
             </a>
-            <a href="#" className="icon stackoverflow" title="StackOverflow">
+            <a href="https://pt.stackoverflow.com/users/293325/igor-castro" target="_blank" className="icon stackoverflow" title="StackOverflow">
               <svg viewBox="0 0 512 512">
                 <path d="M294.8 361.2l-122 0.1 0-26 122-0.1L294.8 361.2zM377.2 213.7L356.4 93.5l-25.7 4.5 20.9 120.2L377.2 213.7zM297.8 301.8l-121.4-11.2 -2.4 25.9 121.4 11.2L297.8 301.8zM305.8 267.8l-117.8-31.7 -6.8 25.2 117.8 31.7L305.8 267.8zM321.2 238l-105-62 -13.2 22.4 105 62L321.2 238zM346.9 219.7l-68.7-100.8 -21.5 14.7 68.7 100.8L346.9 219.7zM315.5 275.5v106.5H155.6V275.5h-20.8v126.9h201.5V275.5H315.5z"/>
               </svg>
             </a>
-            <a href="#" className="icon linkedin" title="LinkedIn">
+            <a href="https://www.linkedin.com/in/igorncastro/" target="_blank" className="icon linkedin" title="LinkedIn">
               <svg viewBox="0 0 512 512">
                 <path d="M186.4 142.4c0 19-15.3 34.5-34.2 34.5 -18.9 0-34.2-15.4-34.2-34.5 0-19 15.3-34.5 34.2-34.5C171.1 107.9 186.4 123.4 186.4 142.4zM181.4 201.3h-57.8V388.1h57.8V201.3zM273.8 201.3h-55.4V388.1h55.4c0 0 0-69.3 0-98 0-26.3 12.1-41.9 35.2-41.9 21.3 0 31.5 15 31.5 41.9 0 26.9 0 98 0 98h57.5c0 0 0-68.2 0-118.3 0-50-28.3-74.2-68-74.2 -39.6 0-56.3 30.9-56.3 30.9v-25.2H273.8z"/>
               </svg>
@@ -337,22 +376,22 @@ function App() {
           </div>
           <h5 className="mx-2 mt-2 font-medium text-gray-800 dark:text-gray-200 text-center">Contact me</h5>
           <div className="soc">
-            <a href="#" className="icon facebook" title="Facebook">
+            <a href="https://www.facebook.com/castroigor" target="_blank" className="icon facebook" title="Facebook">
               <svg viewBox="0 0 512 512">
                 <path d="M211.9 197.4h-36.7v59.9h36.7V433.1h70.5V256.5h49.2l5.2-59.1h-54.4c0 0 0-22.1 0-33.7 0-13.9 2.8-19.5 16.3-19.5 10.9 0 38.2 0 38.2 0V82.9c0 0-40.2 0-48.8 0 -52.5 0-76.1 23.1-76.1 67.3C211.9 188.8 211.9 197.4 211.9 197.4z"/>
               </svg>
             </a>
-            <a href="#" className="icon github" title="GitHub">
+            <a href="https://github.com/IgorNCastro" target="_blank" className="icon github" title="GitHub">
               <svg viewBox="71 71 370 370">
                 <path d="M256 70.7c-102.6 0-185.9 83.2-185.9 185.9 0 82.1 53.3 151.8 127.1 176.4 9.3 1.7 12.3-4 12.3-8.9V389.4c-51.7 11.3-62.5-21.9-62.5-21.9 -8.4-21.5-20.6-27.2-20.6-27.2 -16.9-11.5 1.3-11.3 1.3-11.3 18.7 1.3 28.5 19.2 28.5 19.2 16.6 28.4 43.5 20.2 54.1 15.4 1.7-12 6.5-20.2 11.8-24.9 -41.3-4.7-84.7-20.6-84.7-91.9 0-20.3 7.3-36.9 19.2-49.9 -1.9-4.7-8.3-23.6 1.8-49.2 0 0 15.6-5 51.1 19.1 14.8-4.1 30.7-6.2 46.5-6.3 15.8 0.1 31.7 2.1 46.6 6.3 35.5-24 51.1-19.1 51.1-19.1 10.1 25.6 3.8 44.5 1.8 49.2 11.9 13 19.1 29.6 19.1 49.9 0 71.4-43.5 87.1-84.9 91.7 6.7 5.8 12.8 17.1 12.8 34.4 0 24.9 0 44.9 0 51 0 4.9 3 10.7 12.4 8.9 73.8-24.6 127-94.3 127-176.4C441.9 153.9 358.6 70.7 256 70.7z"/>
               </svg>
             </a>
-            <a href="#" className="icon stackoverflow" title="StackOverflow">
+            <a href="https://pt.stackoverflow.com/users/293325/igor-castro" target="_blank" className="icon stackoverflow" title="StackOverflow">
               <svg viewBox="0 0 512 512">
                 <path d="M294.8 361.2l-122 0.1 0-26 122-0.1L294.8 361.2zM377.2 213.7L356.4 93.5l-25.7 4.5 20.9 120.2L377.2 213.7zM297.8 301.8l-121.4-11.2 -2.4 25.9 121.4 11.2L297.8 301.8zM305.8 267.8l-117.8-31.7 -6.8 25.2 117.8 31.7L305.8 267.8zM321.2 238l-105-62 -13.2 22.4 105 62L321.2 238zM346.9 219.7l-68.7-100.8 -21.5 14.7 68.7 100.8L346.9 219.7zM315.5 275.5v106.5H155.6V275.5h-20.8v126.9h201.5V275.5H315.5z"/>
               </svg>
             </a>
-            <a href="#" className="icon linkedin" title="LinkedIn">
+            <a href="https://www.linkedin.com/in/igorncastro/" target="_blank" className="icon linkedin" title="LinkedIn">
               <svg viewBox="0 0 512 512">
                 <path d="M186.4 142.4c0 19-15.3 34.5-34.2 34.5 -18.9 0-34.2-15.4-34.2-34.5 0-19 15.3-34.5 34.2-34.5C171.1 107.9 186.4 123.4 186.4 142.4zM181.4 201.3h-57.8V388.1h57.8V201.3zM273.8 201.3h-55.4V388.1h55.4c0 0 0-69.3 0-98 0-26.3 12.1-41.9 35.2-41.9 21.3 0 31.5 15 31.5 41.9 0 26.9 0 98 0 98h57.5c0 0 0-68.2 0-118.3 0-50-28.3-74.2-68-74.2 -39.6 0-56.3 30.9-56.3 30.9v-25.2H273.8z"/>
               </svg>
